4e1241610c64af34cc6176151d5ab3adbbead685
vivek <vivekhonule@gmail.com>
2017-08-27 15:14:35 +0530

    added commits

--- src/main/java/SumOfProperDivisor.java
+++ src/main/java/SumOfProperDivisor.java
@@ -1,5 +1,9 @@
 import java.util.Scanner;
 
+/*
+vivekhonule
+ */
+
 public class SumOfProperDivisor {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
